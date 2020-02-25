30f5f1ce2e1580c6cc72a6e80f8cfe7e4d5b2a2a
Lester Young <lyoung070789@gmail.com>
2020-02-25 20:40:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Artist
   attr_accessor :name
   attr_reader :songs
-  extend Memorable:ClassMethods
+  extend Memorable::ClassMethods
   include Paramable
   include Memorable::InstanceMethods
   
